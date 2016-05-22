37ab13c5cf007f78c808118828e600fdb4a4dc45
F. Eduardo Ascencio D <lypef@localhost.localdomain>
2016-05-22 18:30:59 -0500

    work

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class clients(models.Model): 
+    nombre = models.CharField(max_length=60) 
+    empresa = models.CharField(max_length=60) 
+    direccion = models.CharField(max_length=60) 
+    telefono = models.CharField(max_length=60) 
+    movil = models.CharField(max_length=60) 
+    email = models.EmailField()
+
--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 
-from django import forms
 
 # Create your views here.
 def login(request): 
@@ -22,7 +21,24 @@ def manage(request):
 
 @login_required
 def newclient(request):
+
+    errors = []
     if request.method == 'POST':
-    	return render(request,'manage.html')
-    
-    return render (request,'newclient.html')
+    	if not request.POST.get('nombre', ''): 
+    		errors.append('Falta nombre de cliente.') 
+    	if not request.POST.get('empresa', ''): 
+    		errors.append('Falta el nombre de la empresa.')
+    	
+           	
+    	if not errors:
+    		return HttpResponse('Cliente agregado con exito.') 
+
+    	
+    return render(request, 'newclient.html', {'errors': errors, 
+        'nombre': request.POST.get('nombre', ''),
+        'empresa': request.POST.get('empresa', ''),
+        'direccion': request.POST.get('direccion', ''),
+        'telefono': request.POST.get('telefono', ''),
+        'movil': request.POST.get('movil', ''),
+        'email': request.POST.get('email', '')
+        }) 
