d58cc8962994f4f4f2edb92e5381d9fefc226349
Francisco Eduardo Ascencio Dominguez <Isc@AirdeFrancisco.lan>
2016-06-17 20:49:31 -0500

    heroku update3

--- TonerSystem/settings.py
+++ TonerSystem/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = '/home/lypef/TonerSystem/public'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public'),
 STATIC_URL = '/static/'
 
 
