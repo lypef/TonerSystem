bb84638fb6580186c4dcaabf49c52eff2d8130ec
F. Eduardo Ascencio D <lypef@localhost.localdomain>
2016-06-19 22:42:18 -0500

    db sqlite - alwaysdata

--- TonerSystem/settings.py
+++ TonerSystem/settings.py
@@ -82,8 +82,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/public')
+STATIC_URL = '/home/lypef/TonerSystem/public'
 
 
 from django.core.urlresolvers import reverse_lazy
