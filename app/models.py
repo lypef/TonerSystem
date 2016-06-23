ac8b8913eb84a5347fee82988149fe7e37718da1
Francisco Eduardo Ascencio Dominguez <Isc@AirdeFrancisco.lan>
2016-06-22 19:35:11 -0500

    modificaciones tables

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class clients(models.Model): 
-    nombre = models.CharField(max_length=60) 
+    nombre = models.CharField(max_length=60)
     empresa = models.CharField(max_length=60) 
     direccion = models.CharField(max_length=60) 
     telefono = models.CharField(max_length=60) 
